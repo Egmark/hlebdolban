edf6a2170e977d03b73d092d40110bd99d65711d
Egmark <markarovegor@yandex.ru>
2021-07-21 19:15:23 +0300

    ffffff0

--- шпкф.py
+++ шпкф.py
@@ -1,18 +1,19 @@
 import random
 import pygame
 import time
-
+import mixer
 l = 1
-
+pygame.init()
 did = "Desert Eagle | Директива"
 scar = "SCAR-20 | Наемникs"
 stattrackvoy1 = "StatTrak™ M4A4 | Вой"
 dlor = "AWP | История о драконе"
 neonrevolution = "AK-47 | Неоновая революция"
 buzzkill = "USP-S | Подтвержденное убийство"
-
+sound = pygame.mixer.Sound("Electroman.wav")
+sound.set_volume(1)
 # screen = pygame.display.set_mode((800, 600))
-menu = pygame.image.load("Безымянный.png")
+menu = pygame.image.load("pop.png")
 menu = pygame.transform.scale(menu, (800, 600))
 loading = pygame.image.load("thumb-1920-895715.png")
 loading = pygame.transform.scale(loading, (800, 600))
@@ -50,22 +51,30 @@ screen = pygame.display.set_mode((800, 600))
 # t1=random.choice(gun_list)
 # x=0
 # t3=random.choice(gun_list)
-dragonlore = pygame.image.load("image2.png")
-dragonlore = pygame.transform.scale(dragonlore, (100, 52))
+otkrit_snova=pygame.image.load("Открыть снова.png")
+otkrit_snova = pygame.transform.scale(otkrit_snova, (315, 64))
+dragonlore2 = pygame.image.load("image2.png")
+dragonlore = pygame.transform.scale(dragonlore2, (100, 52))
+dragonlore2 = pygame.transform.scale(dragonlore2, (800, 400))
 stena = pygame.image.load("1593698890_6-p-kirpichnii-fon-11.jpg")
 stena = pygame.transform.scale(stena, (800, 600))
-directiva = pygame.image.load("s802.png")
-directiva = pygame.transform.scale(directiva, (100, 51))
-voy = pygame.image.load("image4.png")
-voy = pygame.transform.scale(voy, (103, 50))
-neonr = pygame.image.load("s799.png")
-neonr = pygame.transform.scale(neonr, (101, 50))
-kill = pygame.image.load("image6.png")
-kill = pygame.transform.scale(kill, (100, 50))
+directiva2 = pygame.image.load("s802.png")
+directiva = pygame.transform.scale(directiva2, (100, 51))
+directiva2 = pygame.transform.scale(directiva2, (600, 400))
+voy2 = pygame.image.load("image4.png")
+voy = pygame.transform.scale(voy2, (103, 50))
+voy2 = pygame.transform.scale(voy2, (800, 400))
+neonr2 = pygame.image.load("s799.png")
+neonr = pygame.transform.scale(neonr2, (101, 50))
+neonr2 = pygame.transform.scale(neonr2, (800, 400))
+kill2 = pygame.image.load("image6.png")
+kill = pygame.transform.scale(kill2, (100, 50))
+kill2 = pygame.transform.scale(kill2, (600, 400))
 vopr = pygame.image.load("kartinki-znak-voprosa-4.jpg")
 vopr = pygame.transform.scale(vopr, (600, 400))
-digl = pygame.image.load("дигл.png")
-digl = pygame.transform.scale(digl, (102, 50))
+digl2 = pygame.image.load("дигл.png")
+digl = pygame.transform.scale(digl2, (102, 50))
+digl2 = pygame.transform.scale(digl2, (600, 400))
 gun_list = [dragonlore ,voy, directiva, neonr, kill, digl]
 t = dragonlore
 t2 = random.choice(gun_list)
@@ -93,9 +102,18 @@ y3 = 499
 y4 = 528
 # screen2 =pygame.display.set_mode((800,600))
 pygame.display.flip()
-print(random_gun[18])
-
-
+if random_gun[18] == dragonlore:
+    dropl = dragonlore2
+if random_gun[18] == directiva:
+    dropl = directiva2
+if random_gun[18] == kill:
+    dropl = kill2
+if random_gun[18] == voy:
+    dropl = voy2
+if random_gun[18] == neonr:
+    dropl = neonr2
+if random_gun[18] == digl:
+    dropl = digl2
 def crutilka(x):
     sec = 0
 
@@ -133,8 +151,9 @@ x = 0
 crut = False
 sec = 0
 menu1 = True
+again = False
 while True:
-
+    sound.play()
     pos = pygame.mouse.get_pos()
 
     # screen.blit(stena, (0, 0))
@@ -142,6 +161,42 @@ while True:
         if e.type == pygame.QUIT:
             exit()
         if e.type == pygame.MOUSEBUTTONDOWN:
+            if 0 < pos[0] < 315 and 500 < pos[1] < 564 and again:
+                print('egor tuk')
+                x=0
+                random_gun = [dragonlore for _ in range(3)]
+                random_gun += [voy for _ in range(7)]
+                random_gun += [directiva for _ in range(32)]
+                random_gun += [kill for _ in range(25)]
+                random_gun += [neonr for _ in range(10)]
+                random_gun += [digl for _ in range(25)]
+                random.shuffle(random_gun)
+
+                if random_gun[18] == dragonlore:
+                    dropl = dragonlore2
+                if random_gun[18] == directiva:
+                    dropl = directiva2
+                if random_gun[18] == kill:
+                    dropl = kill2
+                if random_gun[18] == voy:
+                    dropl = voy2
+                if random_gun[18] == neonr:
+                    dropl = neonr2
+                if random_gun[18] == digl:
+                    dropl = digl2
+
+
+
+
+                crut = True
+
+                menu1 = False
+                screen.blit(chfon2, (0, 0))
+                screen.blit(dropl, (50, 100))
+                screen.blit(otkrit_snova, (0, 500))
+
+
+                pygame.display.flip()
             if x3 < pos[0] < x4 and y3 < pos[1] < y4 and menu1:
                 x1 = 0
                 x2 = 0
@@ -165,6 +220,13 @@ while True:
                 menu1 = False
 
                 pygame.display.flip()
+            elif x3 < pos[0] < x4 and y3 < pos[1] < y4 and not menu1:
+
+                crut = True
+
+                menu1 = False
+
+                pygame.display.flip()
 
     if crut:
         sec += l
@@ -174,9 +236,32 @@ while True:
         crutilka(x)
 
         if sec > 980:
-            time.sleep(5)
+            time.sleep(2)
             crut = False
-            l = 1
+            sec = 0
+            x = 0
+            #dropl = pygame.transform.scale(random_gun[18], (500, 300))
+            screen.blit(chfon2, (0, 0))
+            screen.blit(dropl, (50, 100))
+            pygame.display.flip()
+            time.sleep(0.1)
+            screen.blit(otkrit_snova,(0,500))
+            again = True
+
+        #     x=0
+        #     random_gun = [dragonlore for _ in range(3)]
+        #     random_gun += [voy for _ in range(7)]
+        #     random_gun += [directiva for _ in range(32)]
+        #     random_gun += [kill for _ in range(25)]
+        #     random_gun += [neonr for _ in range(10)]
+        #     random_gun += [digl for _ in range(25)]
+        #     random.shuffle(random_gun)
+        #
+        #     crut = True
+        #
+        #     menu1 = False
+        #
+        #     pygame.display.flip()
 
     pygame.time.delay(int(round(sec * 0.0)))
     pygame.display.flip()
